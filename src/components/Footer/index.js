f38823ed6a8ceae1781ec15c3172a1aa0e14ec3a
Your Name <moseoduor86@gmail.com>
2021-06-06 21:01:27 +0300

    footer changes

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -35,6 +35,12 @@ const Footer = ({ t }) => {
           <Container>
             <Row type="flex" justify="space-between">
               <Col lg={10} md={10} sm={12} xs={24}>
+                <S.Language>{t("ADDRESS")}</S.Language>
+                <S.Para>Rancho Santa Margarita</S.Para>
+                <S.Para>2131 Elk Street</S.Para>
+                <S.Para>California</S.Para>
+              </Col>
+              <Col lg={10} md={10} sm={12} xs={24}>
                 <S.Language>{t("Contact")}</S.Language>
                 <S.Large to="/">{t("Tell us everything")}</S.Large>
                 <S.Para>
@@ -46,61 +52,6 @@ const Footer = ({ t }) => {
                   <S.Chat>{t(`Let's Chat`)}</S.Chat>
                 </a>
               </Col>
-              <Col lg={8} md={8} sm={12} xs={24}>
-                <S.Title>{t("Policy")}</S.Title>
-                <S.Large to="/" left="true">
-                  {t("Application Security")}
-                </S.Large>
-                <S.Large left="true" to="/">
-                  {t("Software Principles")}
-                </S.Large>
-              </Col>
-              <Col lg={6} md={6} sm={12} xs={24}>
-                <S.Empty />
-                <S.Large left="true" to="/">
-                  {t("Support Center")}
-                </S.Large>
-                <S.Large left="true" to="/">
-                  {t("Customer Support")}
-                </S.Large>
-              </Col>
-            </Row>
-            <Row type="flex" justify="space-between">
-              <Col lg={10} md={10} sm={12} xs={24}>
-                <S.Empty />
-                <S.Language>{t("ADDRESS")}</S.Language>
-                <S.Para>Rancho Santa Margarita</S.Para>
-                <S.Para>2131 Elk Street</S.Para>
-                <S.Para>California</S.Para>
-              </Col>
-              <Col lg={8} md={8} sm={12} xs={24}>
-                <S.Title>{t("Company")}</S.Title>
-                <S.Large left="true" to="/">
-                  {t("About")}
-                </S.Large>
-                <S.Large left="true" to="/">
-                  {t("Blog")}
-                </S.Large>
-                <S.Large left="true" to="/">
-                  {t("Press")}
-                </S.Large>
-                <S.Large left="true" to="/">
-                  {t("Careers & Culture")}
-                </S.Large>
-              </Col>
-              <Col lg={6} md={6} sm={12} xs={24}>
-                <S.Select>
-                  <S.Label htmlFor="select-lang">{t("Language")}</S.Label>
-                  <S.LangSelect
-                    onChange={handleChange}
-                    value={i18n.language}
-                    id="select-lang"
-                  >
-                    <option value="en">English</option>
-                    <option value="es">Español</option>
-                  </S.LangSelect>
-                </S.Select>
-              </Col>
             </Row>
           </Container>
         </S.Footer>
@@ -110,12 +61,16 @@ const Footer = ({ t }) => {
               type="flex"
               justify="space-between"
               align="middle"
-              style={{ paddingTop: "3rem" }}
+              style={{
+                paddingTop: "3rem",
+                paddingRight: "4rem",
+                paddingLeft: "2rem",
+              }}
             >
               <S.NavLink to="/">
                 <S.LogoContainer>
                   <SvgIcon
-                    src="logo.svg"
+                    src="logo1.svg"
                     aria-label="homepage"
                     width="101px"
                     height="64px"
